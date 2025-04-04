53d525fce80dda140a7fd5eeccfbccba1fb75054
Milan <MilanV2005@outlook.com>
2025-04-04 19:08:05 +0200

    ...

--- src/main/java/nl/multitime/multiSite/MultiSite.java
+++ src/main/java/nl/multitime/multiSite/MultiSite.java
@@ -1,17 +1,72 @@
 package nl.multitime.multiSite;
 
+import nl.multitime.multiSite.api.RestApiServer;
+import nl.multitime.multiSite.commands.AdminCommand;
+import nl.multitime.multiSite.config.ConfigManager;
+import nl.multitime.multiSite.data.DataManager;
+import nl.multitime.multiSite.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public final class MultiSite extends JavaPlugin {
 
+    private ConfigManager configManager;
+    private DataManager dataManager;
+    private RestApiServer apiServer;
+    private BukkitTask statsTask;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
 
+        this.configManager = new ConfigManager(this);
+        this.dataManager = new DataManager(this);
+
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+
+        getCommand("multisite").setExecutor(new AdminCommand(this));
+
+        int port = getConfig().getInt("api.port", 8080);
+        this.apiServer = new RestApiServer(this, port);
+        apiServer.start();
+
+        int updateInterval = getConfig().getInt("data.update-interval", 60) * 20; // Convert to ticks
+        this.statsTask = getServer().getScheduler().runTaskTimerAsynchronously(
+                this,
+                () -> dataManager.updateServerStats(),
+                100L,
+                updateInterval
+        );
+
+        getLogger().info("MultiSite has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (statsTask != null) {
+            statsTask.cancel();
+        }
+
+        if (apiServer != null) {
+            apiServer.stop();
+        }
+
+        if (dataManager != null) {
+            dataManager.saveAllData();
+        }
+
+        getLogger().info("MultiSite has been disabled!");
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public RestApiServer getApiServer() {
+        return apiServer;
     }
 }
--- src/main/java/nl/multitime/multiSite/api/PlayerHandler.java
+++ src/main/java/nl/multitime/multiSite/api/PlayerHandler.java
@@ -1,4 +1,118 @@
 package nl.multitime.multiSite.api;
 
-public class PlayerHandler {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import nl.multitime.multiSite.MultiSite;
+import nl.multitime.multiSite.data.PlayerData;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class PlayerHandler implements HttpHandler {
+
+    private final MultiSite plugin;
+
+    public PlayerHandler(MultiSite plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type,Authorization");
+
+        if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+            exchange.sendResponseHeaders(204, -1);
+            return;
+        }
+
+        if (!exchange.getRequestMethod().equalsIgnoreCase("GET")) {
+            String response = "Method not allowed";
+            exchange.sendResponseHeaders(405, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            return;
+        }
+
+        String query = exchange.getRequestURI().getQuery();
+        String playerIdentifier = null;
+
+        if (query != null) {
+            String[] params = query.split("&");
+            for (String param : params) {
+                String[] keyValue = param.split("=");
+                if (keyValue.length == 2) {
+                    if (keyValue[0].equals("uuid") || keyValue[0].equals("name")) {
+                        playerIdentifier = keyValue[1];
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (playerIdentifier == null) {
+            String response = "Missing player identifier (uuid or name)";
+            exchange.sendResponseHeaders(400, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            return;
+        }
+
+        OfflinePlayer offlinePlayer = null;
+
+        try {
+            UUID uuid = UUID.fromString(playerIdentifier);
+            offlinePlayer = Bukkit.getOfflinePlayer(uuid);
+        } catch (IllegalArgumentException e) {
+            offlinePlayer = Bukkit.getOfflinePlayer(playerIdentifier);
+        }
+
+        if (offlinePlayer == null || !offlinePlayer.hasPlayedBefore()) {
+            String response = "Player not found";
+            exchange.sendResponseHeaders(404, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            return;
+        }
+
+        PlayerData playerData = plugin.getDataManager().getPlayerData(offlinePlayer.getUniqueId());
+
+        JSONObject response = new JSONObject();
+        response.put("uuid", offlinePlayer.getUniqueId().toString());
+        response.put("name", offlinePlayer.getName());
+        response.put("online", offlinePlayer.isOnline());
+        response.put("firstPlayed", offlinePlayer.getFirstPlayed());
+        response.put("lastPlayed", offlinePlayer.getLastPlayed());
+        response.put("playTime", playerData.getPlayTime());
+        response.put("kills", playerData.getKills());
+        response.put("deaths", playerData.getDeaths());
+        response.put("lastLogin", playerData.getLastLogin());
+        response.put("lastLogout", playerData.getLastLogout());
+
+        if (offlinePlayer.isOnline()) {
+            Player player = offlinePlayer.getPlayer();
+            response.put("health", player.getHealth());
+            response.put("level", player.getLevel());
+            response.put("exp", player.getExp());
+            response.put("gameMode", player.getGameMode().toString());
+            response.put("world", player.getWorld().getName());
+        }
+
+        String responseText = response.toJSONString();
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, responseText.length());
+
+        OutputStream os = exchange.getResponseBody();
+        os.write(responseText.getBytes());
+        os.close();
+    }
 }
